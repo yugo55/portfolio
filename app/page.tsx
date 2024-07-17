b9317f73e76d328a83aad0442894a6302bbc934f
yamauchiyugo <yugo.yamauchi@rooter.co.jp>
2024-07-17 17:42:41 +0900

    シングルクォーテーション修正

--- app/page.tsx
+++ app/page.tsx
@@ -5,7 +5,7 @@ import { motion } from "framer-motion";
 export default function Page() {
   return (
     <main className="relative overflow-hidden">
-      <h1 className="text-8xl h-screen flex items-center px-14 leading-snug font-bold">YUGO'S<br />PORTFOLIO</h1>
+      <h1 className="text-8xl h-screen flex items-center px-14 leading-snug font-bold">{`YUGO'S`}<br />{`PORTFOLIO`}</h1>
       <motion.img src="./portfolio_hexagon.png" className="absolute -z-10 -top-52 -left-52" animate={{ rotate: [0, 360] }} transition={{ duration: 40, repeat: Infinity, ease: "linear" }} />
       <motion.img src="./portfolio_hexagon.png" className="absolute -z-10 w-3/6 top-96 -right-52 overflow-x-hidden" animate={{ rotate: [0, 360] }} transition={{ duration: 70, repeat: Infinity, ease: "linear" }} />
     </main>
--- app/ui/header.tsx
+++ app/ui/header.tsx
@@ -3,7 +3,7 @@ import Link from "next/link";
 export default function Header() {
   return (
     <div className="flex justify-between px-14 py-4 w-screen fixed z-10">
-      <Link href="/" className="font-semibold">Yugo's Portfolio</Link>
+      <Link href="/" className="font-semibold">{`Yugo's Portfolio`}</Link>
       <nav>
         <ul className="flex">
           <li><Link href="/about">about</Link></li>
