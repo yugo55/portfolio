a37b7592008d9c22ed4f9ee35dd2e8a7cf691088
yamauchiyugo <yugo.yamauchi@rooter.co.jp>
2025-06-25 21:42:53 +0900

    aboutの学年修正

--- app/about/page.tsx
+++ app/about/page.tsx
@@ -7,12 +7,27 @@ export default function Page() {
       <section className="flex relative">
         <div className="max-w-[50%]">
           <h3 className="text-5xl mb-4">山内 悠剛</h3>
-          <p>早稲田大学文学部 2年生</p>
+          <p>早稲田大学文学部 3年生</p>
           <p>東京都生まれ</p>
-          <p>大学1年生からプログラミングを始める。早稲田大学のプログラミングサークルに入り、HTML, CSSから勉強を進める。1年生の夏には早稲田祭のイベントのホームページ制作に携わり、秋からはネイティブアプリ制作を経験。インターンではスクレイピング実務を経験。</p>
+          <p>
+            大学1年生からプログラミングを始める。早稲田大学のプログラミングサークルに入り、HTML,
+            CSSから勉強を進める。1年生の夏には早稲田祭のイベントのホームページ制作に携わり、秋からはネイティブアプリ制作を経験。インターンではスクレイピング実務を経験。
+          </p>
         </div>
-        <Image src="/logpose.png" alt="" width={300} height={100} className="pl-8" />
-        <Image src="/free.png" alt="" width={500} height={100} className="pl-8 absolute right-2.5 bottom-7" />
+        <Image
+          src="/logpose.png"
+          alt=""
+          width={300}
+          height={100}
+          className="pl-8"
+        />
+        <Image
+          src="/free.png"
+          alt=""
+          width={500}
+          height={100}
+          className="pl-8 absolute right-2.5 bottom-7"
+        />
       </section>
     </div>
   );
